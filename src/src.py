c194e8f98d0f1697b32a73bd97c00d19ef58a60a
zainabmakhdum <90002363+zainabmakhdum@users.noreply.github.com>
2025-12-14 18:53:16 -0500

    Create src.py

---
+++ src/src.py
@@ -0,0 +1,20 @@
+print("delete this file later")
+
+
+"""
+add subfolders for the following:
+
+app/
+  -> app.py (UI)
+  -> template_embeddings.py (builds the final prompt and put everything together)
+  -> llm file (final output by llm)
+
+ingestion/
+  -> (add code for document parsing)
+
+chunking/
+  -> (add code for chunking and embedding)
+
+finetuning-llm/
+  -> (add code for finetuning llm) 
+"""
